147faf27cccb3d15c7e1a4faf7897806de89fe77
Argha <arghagolui@gmail.com>
2023-03-30 00:53:45 +0530

    video 49: hashing password using bcryptjs

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET="arghaisabadb$oy";
 
 //create a user using POST: /api/auth/createuser     login does not require
 
@@ -23,17 +27,33 @@ router.post(
     }
 
     //check whether the user already exists or not
-    let user=await User.findOne({email:req.body.email});
-    if(user){
-        return res.status(400).json({"error":"sorry, this user already exists"});
+    try {
+        let user=await User.findOne({email:req.body.email});
+        if(user){
+            return res.status(400).json({"error":"sorry, this user already exists"});
+        }
+
+        const salt = await bcrypt.genSaltSync(10);
+        let secPass=await bcrypt.hash(req.body.password,salt);
+
+        user = await User.create({
+          name: req.body.name,
+          email: secPass,
+          password: req.body.password,
+        })
+
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const token = jwt.sign(data,JWT_SECRET);
+        res.json(user);
+        console.log(token);
+        
+    } catch (error) {
+        res.status(400).json({"message":error.message});
     }
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-    
-    res.json({"ok":"data is succesfully inserted"});
     
     // .then((user) => res.json(user)).catch((err)=>{
     //     res.json({"error":"you do not use duplicate request","message":err.message});

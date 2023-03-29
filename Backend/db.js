9c488dffb9b35a18e4a6fd284fbfc1e9e2d17ce3
Argha <arghagolui@gmail.com>
2023-03-29 13:38:00 +0530

    video 43: db and express server setup

---
+++ Backend/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+const mongoURI="mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0";
+
+const connectToMongo= ()=>{
+    mongoose.connect(mongoURI).then(()=>{
+        console.log("succesfully connected");
+    }).catch(()=>{
+        console.log("sorry not connect");
+    });
+}
+
+module.exports=connectToMongo;

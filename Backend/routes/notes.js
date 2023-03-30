91dfeb80ae760d8d8a8a1e5753b79ea3cd7f54a0
Argha <arghagolui@gmail.com>
2023-03-30 15:17:03 +0530

    video 52: fetching all notes and add note

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,12 +1,55 @@
 // all notes code are written here
 
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const Notes = require("../models/Notes");
+const fetchuser = require("../middleware/fetchuser");
+const { body, validationResult } = require("express-validator");
 
+//  Route1: Get all notes using : GET /api/notes/fetchallnotes  :Login requied
 
-//all authentication code is written here
-router.get('/',(req,res)=>{
-    res.json([]);
-})
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+    try {
+        const notes = await Notes.find({ user: req.user.id });
+        res.json(notes);
+        
+    } catch (error) {
+        res.status(400).json({"error":error.message});
+    }
+});
 
-module.exports=router
+// Route2: add all notes of the user : POST /api/notes/addnote :Login required
+
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Enter a valid description").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      //if there are error return bad request and the error
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      const savenote = await note.save();
+
+      res.json(savenote);
+    } catch (error) {
+        res.status(400).json({"error":error.message});
+    }
+  }
+);
+
+module.exports = router;

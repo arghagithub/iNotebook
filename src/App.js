28947effaaeafc1334fc74d9c5d2f16c12090149
Argha <arghagolui@gmail.com>
2023-03-31 00:19:05 +0530

    video 56: creating navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h1>Hello , It is iNotebook</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }

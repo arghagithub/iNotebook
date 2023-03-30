02a1bf631d52eb1b5945eb58d9b95e8bc61ed02c
Argha <arghagolui@gmail.com>
2023-03-30 10:51:47 +0530

    video50 :creating login endpoint and sending auth token

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,10 +2,9 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-const JWT_SECRET="arghaisabadb$oy";
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "arghaisabadb$oy";
 
 //create a user using POST: /api/auth/createuser     login does not require
 
@@ -20,41 +19,41 @@ router.post(
     }),
   ],
   async (req, res) => {
-    const errors = validationResult(req);
     //if there are error return bad request and the error
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
     //check whether the user already exists or not
     try {
-        let user=await User.findOne({email:req.body.email});
-        if(user){
-            return res.status(400).json({"error":"sorry, this user already exists"});
-        }
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "sorry, this user already exists" });
+      }
 
-        const salt = await bcrypt.genSaltSync(10);
-        let secPass=await bcrypt.hash(req.body.password,salt);
+      const salt = await bcrypt.genSaltSync(10);
+      let secPass = await bcrypt.hash(req.body.password, salt);
 
-        user = await User.create({
-          name: req.body.name,
-          email: secPass,
-          password: req.body.password,
-        })
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
 
-        const data={
-            user:{
-                id:user.id
-            }
-        }
-        const token = jwt.sign(data,JWT_SECRET);
-        res.json(user);
-        console.log(token);
-        
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, JWT_SECRET);
+      res.json({ token: token });
     } catch (error) {
-        res.status(400).json({"message":error.message});
+      res.status(400).json({ message: error.message });
     }
-    
+
     // .then((user) => res.json(user)).catch((err)=>{
     //     res.json({"error":"you do not use duplicate request","message":err.message});
     //     console.log("Duplicate data can not be inserted");
@@ -66,4 +65,50 @@ router.post(
   }
 );
 
+//authenticate user using POST /api/auth/login  : no login required
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "password can not be blanked").exists(),
+  ],
+  async (req, res) => {
+    //if there are error return bad request and the error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "please try to login with correct credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "please try to login with correct credentials" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, JWT_SECRET);
+      res.json({ "token": token, "message":"succesfully logged in"});
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ "error": "Internal server error" });
+    }
+  }
+);
 module.exports = router;
